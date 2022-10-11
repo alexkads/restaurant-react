714b8e169073b972af379c46b60dfe027ebce44e
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-11 18:27:03 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='bg-red-300 h-full bg-pattern bg-repeat max-w-[1800px] mx-auto'>
+      <div className='pretitle'>Our Team</div>
+      <h2 className='h2'>Who We Are</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ex,
+        modi pariatur consectetur, placeat asperiores harum porro recusandae
+        natus, architecto excepturi quos minima provident? Odio praesentium
+        inventore cumque optio magni.
+      </p>
+      <div className='h-[4000px]'></div>
+    </div>
+  );
 };
 
 export default App;
