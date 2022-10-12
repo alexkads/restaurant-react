e000405d865af80da0e4aaadae780c995a48e699
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-12 18:10:17 +0300

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,46 @@
 import React, { useState } from 'react';
-import NavMobile from './NavMobile';
 
+// import components
+import NavMobile from './NavMobile';
+// import logo
+import LogoWhite from '../assets/img/header/logo-white.png';
+import LogoDark from '../assets/img/header/logo-dark.png';
 // import motion
 import { motion } from 'framer-motion';
 
 const navMobileVariants = {
   hidden: {
+    clipPath: 'circle(6.2% at 1% 1%)',
     opacity: 0,
-    clipPath: 'circle(9.8% at 51% 0)',
+    transition: {
+      delay: 0.5,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
   },
   show: {
     opacity: 1,
-    clipPath: 'circle(89.4% at 51% 0)',
-    transition: { type: 'spring', damping: 20, stifness: 60 },
+    clipPath: `circle(139.5% at 1% 2%)`,
+    transition: {
+      type: 'spring',
+      stiffness: 40,
+    },
+  },
+};
+
+const menuBarRotateLeft = {
+  hidden: { rotate: 0 },
+  show: {
+    rotate: -45,
+    translateY: 2,
+  },
+};
+
+const menuBarRotateRight = {
+  hidden: { rotate: 0 },
+  show: {
+    rotate: 45,
   },
 };
 
@@ -23,9 +51,29 @@ const Header = () => {
       <div className='container mx-auto'>
         <div className='text-white flex justify-between items-center py-[70px]'>
           {/* menu btn */}
-          <div onClick={() => setNavMobile(!navMobile)}>menu btn</div>
+          <div
+            className={`${
+              navMobile ? 'bg-accent gap-y-0' : 'bg-transparent gap-y-2'
+            } flex flex-col items-center justify-center w-12 h-12 `}
+            onClick={() => setNavMobile(!navMobile)}
+          >
+            <motion.div
+              variants={menuBarRotateLeft}
+              initial='hidden'
+              animate={navMobile ? 'show' : ''}
+              className='w-10 h-[2px] bg-white'
+            ></motion.div>
+            <motion.div
+              variants={menuBarRotateRight}
+              initial='hidden'
+              animate={navMobile ? 'show' : ''}
+              className='w-10 h-[2px] bg-white'
+            ></motion.div>
+          </div>
           {/* logo */}
-          <div>logo</div>
+          <div>
+            <img src={LogoWhite} alt='' />
+          </div>
           {/* social icons */}
           <div>social icons</div>
           {/* nav mobile */}
@@ -33,7 +81,7 @@ const Header = () => {
             variants={navMobileVariants}
             initial='hidden'
             animate={navMobile ? 'show' : ''}
-            className='bg-accent w-[300px] absolute h-[80vh] xl:left-[12%] top-[80%] bottom-0 -z-10 rounded-lg'
+            className='bg-accent w-[310px] absolute h-screen xl:left-0 top-0 bottom-0 z-10'
           >
             <NavMobile />
           </motion.div>
--- src/variants.js
+++ src/variants.js
@@ -2,8 +2,8 @@
 export const fadeIn = (direction) => {
   return {
     hidden: {
-      x: direction === 'left' ? 90 : direction === 'right' ? -90 : 0,
-      y: direction === 'up' ? 90 : direction === 'down' ? -90 : 0,
+      x: direction === 'left' ? 80 : direction === 'right' ? -80 : 0,
+      y: direction === 'up' ? 80 : direction === 'down' ? -80 : 0,
       opacity: 0,
     },
     show: {
@@ -11,12 +11,9 @@ export const fadeIn = (direction) => {
       y: 0,
       opacity: 1,
       transition: {
-        type: 'spring',
-        duration: 3,
-        damping: 5,
-        stiffness: 100,
-        // ease: [0.15, 0.15, 0.15, 0.75],
-        // ease: [0, 0.55, 0.45, 1],
+        type: 'tween',
+        duration: 1.1,
+        ease: [0.25, 0.25, 0.25, 0.75],
       },
     },
   };
