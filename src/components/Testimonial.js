c1db12e54fb92aa8344ea9a8f6dbcad4306dffde
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-10-20 10:01:31 +0300

    added

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -3,6 +3,10 @@ import React from 'react';
 import TestimonialCarousel from './TestimonialCarousel';
 // import data
 import { testimonialData } from '../data';
+// import motion
+import { motion } from 'framer-motion';
+// import variants
+import { fadeIn } from '../variants';
 
 const Testimonial = () => {
   // destructure testimonial data
@@ -11,16 +15,26 @@ const Testimonial = () => {
     <div className='bg-testimonial bg-cover bg-no-repeat relative top-[340px] lg:top-[390px] z-10 h-[800px] pt-[60px] md:pt-[120px]'>
       <div className='container mx-auto'>
         {/* text */}
-        <div className='text-center capitalize flex flex-col items-center'>
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.2, 1.6)}
+          initial='hidden'
+          whileInView={'show'}
+          className='text-center capitalize flex flex-col items-center'
+        >
           <h2 className='h2 text-white'>{title}</h2>
           <p className='text-white/70 capitalize mb-8'>{subtitle}</p>
           <div className='mb-12'>
             <img src={modelImg} alt='' />
           </div>
-        </div>
-        <div className='flex justify-center items-center'>
+        </motion.div>
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.4, 1.6)}
+          initial='hidden'
+          whileInView={'show'}
+          className='flex justify-center items-center'
+        >
           <TestimonialCarousel slider={slider} />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
